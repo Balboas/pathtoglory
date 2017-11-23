b0d1b8cf5afddfd928395d4492f63955da4b0a9c
Daniel Baeta <daniel.baeta@gmail.com>
2017-11-23 19:30:21 +0000

    Created message protocol for server. Server worker completed.

---
+++ server-app/src/org/academiadecodigo/balboas/MessageProtocol.java
@@ -0,0 +1,42 @@
+package org.academiadecodigo.balboas;
+
+/**
+ * Created by Daniel Baeta on 23/11/17.
+ */
+public enum MessageProtocol {
+
+    REGISTER("REG"),
+    LOGIN("LOGIN");
+
+    private String protocol;
+    public static final String DELIMITER = "##";
+
+
+    MessageProtocol(String protocol) {
+        this.protocol = protocol;
+    }
+
+    public static String decode(String message) {
+
+        String[] splittedMessage = message.split(DELIMITER);
+        MessageProtocol protocol = MessageProtocol.valueOf(splittedMessage[0]);
+
+        if (protocol == null) {
+            return null;
+        }
+
+        switch (protocol) {
+            case LOGIN:
+                break;
+            case REGISTER:
+                break;
+        }
+        return null;
+    }
+
+    public static String encode(MessageProtocol protocol, String message) {
+
+        return new StringBuilder(protocol.name()).append(DELIMITER).append(message).toString();
+
+    }
+}
--- server-app/src/org/academiadecodigo/balboas/Server.java
+++ server-app/src/org/academiadecodigo/balboas/Server.java
@@ -1,6 +1,9 @@
 package org.academiadecodigo.balboas;
 
+import java.io.BufferedReader;
 import java.io.IOException;
+import java.io.InputStreamReader;
+import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 import java.util.concurrent.ExecutorService;
@@ -11,12 +14,12 @@ import java.util.concurrent.Executors;
  */
 public class Server {
 
-    ServerSocket serverSocket;
-    ExecutorService service;
+    private ServerSocket serverSocket;
+    private ExecutorService service;
     private int portNumber = 32323;
 
 
-    public Server() {
+    Server() {
         init();
     }
 
@@ -56,14 +59,54 @@ public class Server {
 
     private class ServerWorker implements Runnable {
 
-        Socket socket;
+        private Socket socket;
+        private BufferedReader reader;
+        private PrintWriter writer;
 
-        public ServerWorker(Socket socket) {
+
+        ServerWorker(Socket socket) {
             this.socket = socket;
+            init();
+        }
+
+        private void init() {
+
+            try {
+                reader = new BufferedReader(new InputStreamReader(socket.getInputStream()));
+                writer = new PrintWriter(socket.getOutputStream(), true);
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
+
+        private void getMessage(String message) {
+
+            MessageProtocol.decode(message);
+        }
+
+        public void sendMessage(String message){
+
+            writer.println(message);
         }
 
         @Override
         public void run() {
+
+            String message = "";
+
+            try {
+                while ((message = reader.readLine()) != null) {
+                    getMessage(message);
+                }
+            } catch (IOException e) {
+                e.printStackTrace();
+            } finally {
+                try {
+                    socket.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
 
         }
     }
