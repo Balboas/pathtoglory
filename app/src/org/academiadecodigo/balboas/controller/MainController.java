84a7f091658096b9439e4ad532cb1a68ee3318d7
Bruno Portugal <bportugal90@gmail.com>
2017-11-23 17:37:41 +0000

    Navigation class and Controller Interface created

--- app/src/org/academiadecodigo/balboas/controller/MainController.java
+++ app/src/org/academiadecodigo/balboas/controller/MainController.java
@@ -1,4 +1,61 @@
 package org.academiadecodigo.balboas.controller;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+import javafx.scene.control.TextField;
+
+public class MainController implements Controller{
+
+    @FXML
+    private ResourceBundle resources;
+
+    @FXML
+    private URL location;
+
+    @FXML
+    private TextField gymField;
+
+    @FXML
+    private TextField dietField;
+
+    @FXML
+    private TextField beerField;
+
+    @FXML
+    private TextField smokeField;
+
+    @FXML
+    private Button backButton;
+
+    @FXML
+    private Button sendButton;
+
+    @FXML
+    void onBackButton(ActionEvent event) {
+
+        Navigation.getInstance().back();
+    }
+
+    @FXML
+    void onSendButton(ActionEvent event) {
+
+
+
+
+    }
+
+    @FXML
+    void initialize() {
+        assert gymField != null : "fx:id=\"gymField\" was not injected: check your FXML file 'mainView.fxml'.";
+        assert dietField != null : "fx:id=\"dietField\" was not injected: check your FXML file 'mainView.fxml'.";
+        assert beerField != null : "fx:id=\"beerField\" was not injected: check your FXML file 'mainView.fxml'.";
+        assert smokeField != null : "fx:id=\"smokeField\" was not injected: check your FXML file 'mainView.fxml'.";
+        assert backButton != null : "fx:id=\"backButton\" was not injected: check your FXML file 'mainView.fxml'.";
+        assert sendButton != null : "fx:id=\"sendButton\" was not injected: check your FXML file 'mainView.fxml'.";
+
+    }
 }
+
---
+++ app/src/org/academiadecodigo/balboas/controller/Navigation.java
@@ -0,0 +1,112 @@
+package org.academiadecodigo.balboas.controller;
+
+
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.Map;
+
+public final class Navigation {
+
+    public final static int MIN_WIDTH = 1024;
+    public final static int MIN_HEIGHT = 768;
+
+    public final static String VIEW_PATH = "";
+    private static Navigation instance;
+    private LinkedList<Scene> scenes = new LinkedList<>();
+    private Map<String, Controller> controllers = new HashMap<>();
+    private Stage stage;
+
+    private Navigation() {
+    }
+
+    public static Navigation getInstance() {
+
+        if (instance == null) {
+            instance = new Navigation();
+        }
+
+        return instance;
+
+    }
+
+    public void setStage(Stage stage) {
+        this.stage = stage;
+    }
+
+    public Controller getController(String view) {
+        return controllers.get(view);
+    }
+
+    public void loadScreen(String view) {
+
+        // Instantiate the view and the controller
+        Parent root = loadView(view);
+
+        // Create a new scene and add it to the stack
+        Scene scene = new Scene(root, MIN_WIDTH, MIN_HEIGHT);
+        scenes.push(scene);
+
+        // Put the scene on the stage
+        setScene(scene);
+
+    }
+
+    public Parent loadView(String view) {
+
+        Parent root = null;
+
+        try {
+
+            // Instantiate the the controller
+            FXMLLoader fxmlLoader;
+            fxmlLoader = new FXMLLoader(getClass().getResource(VIEW_PATH + "/" + view + ".fxml"));
+            root = fxmlLoader.load();
+
+            // Store the view and the controller
+            controllers.put(view, fxmlLoader.<Controller>getController());
+
+        } catch (IOException e) {
+            System.out.println("Failure to load view " + view + " : " + e.getMessage());
+            e.printStackTrace();
+        }
+
+        return root;
+
+    }
+
+    public void back() {
+
+        if (scenes.isEmpty()) {
+            return;
+        }
+
+        // remove the current scene from the stack
+        scenes.pop();
+
+        // load a new scene from the top of the stack
+        setScene(scenes.peek());
+
+    }
+
+    private void setScene(Scene scene) {
+
+        // set the scene
+        stage.setScene(scene);
+
+        // show the stage
+        stage.show();
+    }
+
+    public void close() {
+
+        stage.close();
+
+    }
+
+}
