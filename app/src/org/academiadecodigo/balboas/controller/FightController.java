d44fab2d757bf06d18a63baa8fcccfde2fd12c7c
Daniel Baeta <daniel.baeta@gmail.com>
2017-11-24 07:13:21 +0000

    Players move with buttons and change image when attacks

--- app/src/org/academiadecodigo/balboas/controller/FightController.java
+++ app/src/org/academiadecodigo/balboas/controller/FightController.java
@@ -4,6 +4,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.KeyEvent;
 
@@ -11,6 +12,9 @@ import javafx.scene.input.KeyEvent;
  * Created by Daniel Baeta on 24/11/17.
  */
 public class FightController {
+
+
+
     @FXML
     private Button fightButton;
 
@@ -27,38 +31,44 @@ public class FightController {
     private Label strengthLabel;
 
     @FXML
-    void beginFight(ActionEvent event) {
+    private Button moveLeftButton;
 
+    @FXML
+    private Button moveRightButton;
+
+    @FXML
+    void fight(ActionEvent event) {
+
+        player1.setImage(new Image("/punching1.png"));
+        if (player1.getX() >= (player2.getX() - player1.getFitWidth())) {
+            //fight.attack();
+            //Vai decrementar a vida do player2
+        }
     }
 
     @FXML
-    void move(KeyEvent event) {
+    void moveLeft(ActionEvent event) {
 
-        System.out.println(event.getCode());
-        switch (event.getCode()) {
-            case LEFT:
-                player1.setX(player1.getX() - 20.0d);
-                if (player1.getX() <= 0) {
-                    player1.setX(0.0d);
-                }
-                break;
-            case RIGHT:
-                player1.setX(player1.getX() + 20.0d);
-                if (player1.getX() >= (player2.getX() - player1.getFitWidth())) {
-                    player1.setX(player2.getX() - player2.getFitWidth());
-                }
-                if (player1.getX() >= 320) {
-                    player1.setX(320.0d);
-                }
-                break;
-            case SPACE:
-                if (player1.getX() >= (player2.getX() - player1.getFitWidth())) {
-                    fight.attack();
-                    //Vai decrementar a vida do player2
-                }
-                break;
+        player1.setImage(new Image("/fighter1Still.png"));
+        player1.setX(player1.getX() - 20.0d);
+        if (player1.getX() <= 0) {
+            player1.setX(0.0d);
         }
     }
+
+    @FXML
+    void moveRight(ActionEvent event) {
+
+        player1.setImage(new Image("/fighter1Still.png"));
+        player1.setX(player1.getX() + 20.0d);
+        if (player1.getX() >= (player2.getX() - player1.getFitWidth())) {
+            player1.setX(player2.getX() - player2.getFitWidth());
+        }
+        if (player1.getX() >= 320) {
+            player1.setX(320.0d);
+        }
+    }
+
 
     public void setHealth(int health){
         healthLabel.setText(String.valueOf(health));
